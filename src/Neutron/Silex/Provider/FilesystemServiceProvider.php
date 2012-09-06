dd4122424871babc9c3719e0bdb087140484867c
Romain Neutron <imprec@gmail.com>
2012-09-06 13:45:35 +0200

    Add Provider

---
+++ src/Neutron/Silex/Provider/FilesystemServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Neutron\Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+class FilesystemServiceProvider implements ServiceProviderInterface
+{
+
+    public function register(Application $app)
+    {
+        $app['filesystem'] = $app->share(function(Application $app) {
+            return new Filesystem();
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
